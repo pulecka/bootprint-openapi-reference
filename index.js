abb83d5532d00a6118e1ef23414a1b71e26c9c0e
Martin Obert <me@pulecka.com>
2017-03-29 17:58:55 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var path = require('path')
+
+/**
+ * Create a bootprint template-module that can be loaded via `Bootprint#load`:
+ *
+ * @name index
+ * @param {BootprintBuilder} builder the current bootprint builder
+ * @return {BootprintBuilder} a bootprint-builder containing the template and Less settings for `bootprint-swagger`
+ * @api public
+ */
+module.exports = function bootprintOpenapiReference(builder) {
+  return builder
+  .load(require('bootprint-openapi'))
+    .merge({
+      'handlebars': {
+        'partials': path.join(__dirname, 'templates/partials'),
+        'helpers': require.resolve('./templates/helpers.js'),
+        'preprocessor': require('./lib/preprocessor.js')
+      },
+      'less': {
+        'main': [
+          require.resolve('./styles/theme.less')
+        ]
+      }
+    })
+}
+
+// Add "package" to be used by bootprint-doc-generator
+module.exports.package = require('./package')
---
+++ templates/helpers.js
@@ -0,0 +1,7 @@
+var _ = require('lodash')
+
+module.exports = {
+  'reference--kebab-case': function (value) {
+    return _.kebabCase(value)
+  }
+}
